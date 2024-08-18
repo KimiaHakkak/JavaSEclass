05967a5ab359e15e536608f504bdb38f9d96261a
KimiaHakkak <kimiaghodsy@gmail.com>
2024-08-18 20:01:33 +0330

    تمرین جلسه 11

--- src/jalase11/model/da/database.sql
+++ src/jalase11/model/da/database.sql
@@ -1,9 +1,10 @@
-CREATE TABLE PRODUCT {
-    id number key number,
-    name ncharvar2(20),
-    brand ncharvar(20),
+CREATE TABLE PRODUCT
+(
+    id    number key number,
+    name  nvarchar2(20),
+    brand nvarchar2(20),
     price number,
     count number
-};
+);
 
 CREATE SEQUENCE PRODUCT_SEQ START WITH 1 INCREMENT BY 1;
